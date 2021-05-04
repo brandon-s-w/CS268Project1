9ea0c1dac57e912ed47fc01f15612683f6249ad4
eblank <eblank1221@gmail.com>
2021-05-04 11:52:30 -0500

    Added all required database forms for project requirements (5 total, 4 meet requirements exactly)

--- AndisCritters.py
+++ AndisCritters.py
@@ -1,27 +1,27 @@
 import base64
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import date
 import sqlite3 as sql
 import os
 
 app = Flask(__name__)
 app.secret_key = 'development key'
 
+con = sql.connect("database.db")
+c = con.cursor()
+
+
+## PAGES
 @app.route('/')
 def home():
     return render_template("index.html")
-
-
 @app.route('/MyHedgehogs')
 def myHedgehogs():
     return render_template("myhedgehogs.html")
-
-
 @app.route('/WatchMeGrow')
 def meGrowing():
     return render_template("megrowing.html")
-
-
 @app.route('/ForSale')
 def forSale():
     con = sql.connect("database.db")
@@ -29,8 +29,6 @@ def forSale():
     rows = getForSale(con, c)
 
     return render_template("forsale.html", rows=rows)
-
-
 @app.route('/Sold')
 def sold():
     con = sql.connect("database.db")
@@ -38,13 +36,9 @@ def sold():
     rows = getSold(con, c)
 
     return render_template("sold.html", rows=rows)
-
-
 @app.route('/Care')
 def care():
     return render_template("care.html")
-
-
 @app.route('/Q&A')
 def questionsAndAnswers():
     con = sql.connect("database.db")
@@ -52,18 +46,16 @@ def questionsAndAnswers():
     rows = getQA(con, c)
 
     return render_template("questionsandanswers.html", rows=rows)
-
-
 @app.route('/FunFacts')
 def funFacts():
-    return render_template("funfacts.html")
+    con = sql.connect("database.db")
+    c = con.cursor()
+    rows = getFacts(con, c)
 
-
+    return render_template("funfacts.html", rows=rows)
 @app.route('/Contact')
 def contact():
     return render_template("contact.html")
-
-
 @app.route('/Reviews')
 def reviews():
     con = sql.connect("database.db")
@@ -71,7 +63,71 @@ def reviews():
     rows = getReviews(con, c)
 
     return render_template("reviews.html", rows=rows)
+@app.route('/submitReview', methods=['POST'])
+def submitReview():
+    review = request.form.get('review')
+    name = request.form.get('name')
+    today = date.today()
+    con = sql.connect("database.db")
+    c = con.cursor()
 
+    c.execute("""INSERT INTO Reviews (name, review, date) VALUES (?, ?, ?)""", (name, review, today.strftime("%m/%d/%y")))
+    # return table results
+    con.row_factory = sql.Row
+    stmt = "select * from Reviews"
+    c.execute(stmt)
+    con.commit()
+    rows=c.fetchall()
+
+    return render_template('reviews.html', rows=rows)
+@app.route('/submitFact', methods=['POST'])
+def submitFact():
+    fact = request.form.get('fact')
+    name = request.form.get('name')
+    today = date.today()
+    con = sql.connect("database.db")
+    c = con.cursor()
+
+    c.execute("""INSERT INTO FunFacts (name, fact, date) VALUES (?, ?, ?)""",
+              (name, fact, today.strftime("%m/%d/%y")))
+    # return table results
+    con.row_factory = sql.Row
+    stmt = "select * from FunFacts"
+    c.execute(stmt)
+    con.commit()
+    rows = c.fetchall()
+
+    return render_template('funfacts.html', rows=rows)
+@app.route('/submitQuestion', methods=['POST'])
+def submitQuestion():
+    question = request.form.get('question')
+    name = request.form.get('name')
+    today = date.today()
+    con = sql.connect("database.db")
+    c = con.cursor()
+
+    c.execute("""INSERT INTO QA (name, question, answer) VALUES (?, ?, ?)""", (name, question, "Not yet answered! if you have an answer, please email it to us!"))
+
+    # return table results
+    con.row_factory = sql.Row
+    stmt = "select * from QA"
+    c.execute(stmt)
+    con.commit()
+    rows = c.fetchall()
+
+    return render_template('questionsandanswers.html', rows=rows)
+@app.route('/inquireHoglet', methods=['POST'])
+def inquireHoglet():
+    email = request.form.get('email')
+    hoglet = request.form.get('hoglet')
+    today = date.today()
+    con = sql.connect("database.db")
+    c = con.cursor()
+
+    c.execute("""INSERT INTO Inquiries (email, hoglet, date) VALUES (?, ?, ?)""", (email, hoglet, today.strftime("%m/%d/%y")))
+    con.commit()
+
+    return render_template('index.html')
 
 class ForSaleListing(object):
     def __init__(self):
@@ -101,119 +157,106 @@ class ForSaleListing(object):
         except Exception as e:
             print(e)
 
+## GETTERS FOR DATABASE STUFF
 def getForSale(con, c):
-    # drop table
-    try:
-        stmt = "DROP TABLE ForSale"
-        c.execute(stmt)
-    except Exception as e:
-        print(e)
-    # create table
-    try:
-        stmt = "CREATE TABLE ForSale (id INTEGER PRIMARY KEY, name TEXT NOT NULL, price TEXT NOT NULL, dob TEXT NOT NULL, color TEXT NOT NULL, gender TEXT NOT NULL, ready TEXT NOT NULL, img TEXT NOT NULL);"
-        c.execute(stmt)
-        con.commit()
-    except Exception as e:
-        print(e)
-
-    # populate table
-    obj = ForSaleListing()
-    forSalePath = os.getcwd() + "\\static\\images\\ForSale"
-    forSalePath = forSalePath.replace("\\", "/")
-    for x in obj.load_directory(path=forSalePath):
-        try:
-            if ".jpg" in x:
-                txtFile = x.replace(".jpg", ".txt")
-                price = dob = color = gender = ready = ''
-                name = x.replace(".jpg", "")
-                try:
-                    with open(forSalePath + "/" + txtFile, "r") as f:
-                        file_data = f.readlines()
-                        for item in file_data:
-                            if 'Price' in item:
-                                price = get_value(item)
-                            elif 'DOB' in item:
-                                dob = get_value(item)
-                            elif 'Color' in item:
-                                color = get_value(item)
-                            elif 'Gender' in item:
-                                gender = get_value(item)
-                            elif 'Ready' in item:
-                                ready = get_value(item)
-                    with open(forSalePath + "/" + x, "rb") as f:
-                        data = base64.b64encode(f.read())
-                        obj.create_database(name=name, price=price, dob=dob, color=color,
-                                            gender=gender, ready=ready, image=data, c=c)
-                    print("Added listing for {}".format(name))
-                except :
-                    print("Incomplete Listing:  {}".format(x))
-
-        except Exception as e:
-            print(e)
-            print("{} Not added to database ".format(x))
-
     # return table results
     con.row_factory = sql.Row
     stmt = "select * from ForSale"
     c.execute(stmt)
 
     return c.fetchall()
-
 def getSold(con, c):
-    # drop table
-    try:
-        stmt = "DROP TABLE Sold"
-        c.execute(stmt)
-    except Exception as e:
-        print(e)
-    # create table
-    try:
-        stmt = "CREATE TABLE Sold (id INTEGER PRIMARY KEY, name TEXT NOT NULL, img TEXT NOT NULL);"
-        c.execute(stmt)
-        con.commit()
-    except Exception as e:
-        print(e)
-
-    # populate table
-    obj = ForSaleListing()
-    soldPath = os.getcwd() + "\\static\\images\\Sold"
-    soldPath = soldPath.replace("\\", "/")
-    for x in obj.load_directory(path=soldPath):
-        try:
-            if ".jpg" in x:
-                name = x.replace(".jpg", "")
-                with open(soldPath + "/" + x, "rb") as f:
-                    data = base64.b64encode(f.read())
-                    c.execute("""INSERT INTO Sold (name, img) VALUES (?, ?)""", (name, data))
-                    print("Sold {} added to database ".format(x))
-        except Exception as e:
-            print(e)
-            print("{} Not added to database ".format(x))
-
     # return table results
     con.row_factory = sql.Row
     stmt = "select * from Sold"
     c.execute(stmt)
 
     return c.fetchall()
+def getFacts(con, c):
+    # return table results
+    con.row_factory = sql.Row
+    stmt = "select * from FunFacts"
+    c.execute(stmt)
 
+    return c.fetchall()
 def getReviews(con, c):
-    # drop table
+    # return table results
+    con.row_factory = sql.Row
+    stmt = "select * from Reviews"
+    c.execute(stmt)
+
+    return c.fetchall()
+def getQA(con, c):
+    # return table results
+    con.row_factory = sql.Row
+    stmt = "select * from QA"
+    c.execute(stmt)
+
+    return c.fetchall()
+def get_value(item):
+    info = item.strip().split(':')
+    val = info[1].strip().split(',')
+    return val[0].strip()
+
+## POPULATE DATABASE
+def popDatabase():
+    # create tables
     try:
-        stmt = "DROP TABLE Reviews"
+        stmt = "CREATE TABLE IF NOT EXISTS ForSale (id INTEGER PRIMARY KEY, name TEXT NOT NULL, price TEXT NOT NULL, dob TEXT NOT NULL, color TEXT NOT NULL, gender TEXT NOT NULL, ready TEXT NOT NULL, img TEXT NOT NULL);"
         c.execute(stmt)
-    except Exception as e:
-        print(e)
-    # create table
-    try:
-        stmt = "CREATE TABLE Reviews (id INTEGER PRIMARY KEY, name TEXT NOT NULL, review TEXT NOT NULL, date TEXT NOT NULL);"
+        stmt = "CREATE TABLE IF NOT EXISTS Sold (id INTEGER PRIMARY KEY, name TEXT NOT NULL, img TEXT NOT NULL);"
+        c.execute(stmt)
+        stmt = "CREATE TABLE IF NOT EXISTS Reviews (id INTEGER PRIMARY KEY, name TEXT NOT NULL, review TEXT NOT NULL, date TEXT NOT NULL);"
+        c.execute(stmt)
+        stmt = "CREATE TABLE If NOT EXISTS QA (id INTEGER PRIMARY KEY, name TEXT NOT NULL, question TEXT NOT NULL, answer TEXT);"
+        c.execute(stmt)
+        stmt = "CREATE TABLE If NOT EXISTS FunFacts (id INTEGER PRIMARY KEY, name TEXT NOT NULL, fact TEXT NOT NULL, date TEXT NOT NULL);"
+        c.execute(stmt)
+        stmt = "CREATE TABLE If NOT EXISTS Inquiries (id INTEGER PRIMARY KEY, email TEXT NOT NULL, hoglet TEXT NOT NULL, date TEXT NOT NULL);"
         c.execute(stmt)
         con.commit()
     except Exception as e:
         print(e)
 
+    # populate tables
     try:
-        # populate table
+        # for sale
+        obj = ForSaleListing()
+        forSalePath = os.getcwd() + "\\static\\images\\ForSale"
+        forSalePath = forSalePath.replace("\\", "/")
+        for x in obj.load_directory(path=forSalePath):
+            try:
+                if ".jpg" in x:
+                    txtFile = x.replace(".jpg", ".txt")
+                    price = dob = color = gender = ready = ''
+                    name = x.replace(".jpg", "")
+                    try:
+                        with open(forSalePath + "/" + txtFile, "r") as f:
+                            file_data = f.readlines()
+                            for item in file_data:
+                                if 'Price' in item:
+                                    price = get_value(item)
+                                elif 'DOB' in item:
+                                    dob = get_value(item)
+                                elif 'Color' in item:
+                                    color = get_value(item)
+                                elif 'Gender' in item:
+                                    gender = get_value(item)
+                                elif 'Ready' in item:
+                                    ready = get_value(item)
+                        with open(forSalePath + "/" + x, "rb") as f:
+                            data = base64.b64encode(f.read())
+                            obj.create_database(name=name, price=price, dob=dob, color=color,
+                                                gender=gender, ready=ready, image=data, c=c)
+                        print("Added listing for {}".format(name))
+                    except:
+                        print("Incomplete Listing:  {}".format(x))
+
+            except Exception as e:
+                print(e)
+                print("{} Not added to database ".format(x))
+
+        # reviews
         obj = ForSaleListing()
         forSalePath = os.getcwd() + "\\static\\Reviews"
         forSalePath = forSalePath.replace("\\", "/")
@@ -235,34 +278,24 @@ def getReviews(con, c):
             except Exception as e:
                 print(e)
                 print("{} Not added to database ".format(x))
-    except Exception as e:
-        print(e)
 
+        # sold
+        obj = ForSaleListing()
+        soldPath = os.getcwd() + "\\static\\images\\Sold"
+        soldPath = soldPath.replace("\\", "/")
+        for x in obj.load_directory(path=soldPath):
+            try:
+                if ".jpg" in x:
+                    name = x.replace(".jpg", "")
+                    with open(soldPath + "/" + x, "rb") as f:
+                        data = base64.b64encode(f.read())
+                        c.execute("""INSERT INTO Sold (name, img) VALUES (?, ?)""", (name, data))
+                        print("Sold {} added to database ".format(x))
+            except Exception as e:
+                print(e)
+                print("{} Not added to database ".format(x))
 
-    # return table results
-    con.row_factory = sql.Row
-    stmt = "select * from Reviews"
-    c.execute(stmt)
-
-    return c.fetchall()
-
-def getQA(con, c):
-    # drop table
-    try:
-        stmt = "DROP TABLE QA"
-        c.execute(stmt)
-    except Exception as e:
-        print(e)
-    # create table
-    try:
-        stmt = "CREATE TABLE QA (id INTEGER PRIMARY KEY, name TEXT NOT NULL, question TEXT NOT NULL, answer TEXT);"
-        c.execute(stmt)
-        con.commit()
-    except Exception as e:
-        print(e)
-
-    try:
-        # populate table
+        # QA
         obj = ForSaleListing()
         qaPath = os.getcwd() + "\\static\\QA"
         qaPath = qaPath.replace("\\", "/")
@@ -282,21 +315,26 @@ def getQA(con, c):
             except Exception as e:
                 print(e)
                 print("{} Not added to database ".format(x))
+        con.commit()
     except Exception as e:
         print(e)
 
-
-    # return table results
-    con.row_factory = sql.Row
-    stmt = "select * from QA"
+    # remove duplicates
+    stmt = "DELETE FROM ForSale WHERE rowid NOT IN (SELECT min(rowid) FROM ForSale GROUP BY name, dob);"
     c.execute(stmt)
+    stmt = "DELETE FROM Sold WHERE rowid NOT IN (SELECT min(rowid) FROM Sold GROUP BY name);"
+    c.execute(stmt)
+    stmt = "DELETE FROM Reviews WHERE rowid NOT IN (SELECT min(rowid) FROM Reviews GROUP BY name);"
+    c.execute(stmt)
+    stmt = "DELETE FROM QA WHERE rowid NOT IN (SELECT min(rowid) FROM QA GROUP BY name);"
+    c.execute(stmt)
+    stmt = "DELETE FROM FunFacts WHERE rowid NOT IN (SELECT min(rowid) FROM FunFacts GROUP BY fact);"
+    c.execute(stmt)
+    stmt = "DELETE FROM Inquiries WHERE rowid NOT IN (SELECT min(rowid) FROM Inquiries GROUP BY email);"
+    c.execute(stmt)
+    con.commit()
 
-    return c.fetchall()
-
-def get_value(item):
-    info = item.strip().split(':')
-    val = info[1].strip().split(',')
-    return val[0].strip()
-
+## START FUNCTION
 if __name__ == '__main__':
+    popDatabase()
     app.run(debug=True)
